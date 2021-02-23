94ca4c293dbed3982ff3af8a66701092639b1e64
Armando Wilmans <armandowjunior@gmail.com>
2021-02-23 16:39:41 -0300

    Commit version passing all tests

--- src/App.js
+++ src/App.js
@@ -15,26 +15,22 @@ library.add(faPlay, faSyncAlt, faPause, faLaptopCode, faCoffee);
 function App() {
   const audioElement = useRef(null);
   const [intervalId, setIntervalId] = useState(null);
-  const [displayTime, setDisplayTime] = useState(25 * 60);
-  const [sessionTime, setSessionTime] = useState(25 * 60);
   const [breakTime, setBreakTime] = useState(5 * 60);
-  const [timerOn, setTimerOn] = useState(false);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
+  const [displayTime, setDisplayTime] = useState(25 * 60);
   const [onBreak, setOnBreak] = useState(false);
 
-  // Listen to the changes in break and session time and update the display time;
   useEffect(() => {
-    if (onBreak) {
-      setDisplayTime(breakTime);
-    }
     if (!onBreak) {
       setDisplayTime(sessionTime);
+    } else if (onBreak) {
+      setDisplayTime(breakTime);
     }
   }, [sessionTime, breakTime]);
 
-  // listen to displaytime changes
-  // it it is zero, play the audio, change session to break or break to session
   useEffect(() => {
     if (displayTime === 0) {
+      audioElement.current.currentTime = 0;
       audioElement.current.play();
       if (!onBreak) {
         setOnBreak(true);
@@ -44,79 +40,71 @@ function App() {
         setDisplayTime(sessionTime);
       }
     }
-  }, [onBreak, sessionTime, breakTime, displayTime]);
+  }, [displayTime, onBreak]);
 
-  const handleStartStopClick = () => {
-    setTimerOn(!timerOn);
-    if (timerOn) {
-      // if the timer is On:
-      // Stop the timer;
-      // clearInterval
+  const timerControl = () => {
+    if (intervalId === null) {
+      const interval = setInterval(() => {
+        setDisplayTime((previousdisplayTime) => previousdisplayTime - 1);
+      }, 1000);
+      setIntervalId(interval);
+    } else {
       clearInterval(intervalId);
       setIntervalId(null);
-    } else {
-      // if the timer is Off:
-      // decrement displaytime by 1 every second (1000ms)
-      // store the id of the interval in a state to be used to clear the interval in the pause
-      const newIntervalId = setInterval(() => {
-        setDisplayTime((prev) => prev - 1);
-      }, 1000);
-      setIntervalId(newIntervalId);
-    }
-  };
-
-  const formatTime = (time, type = "length") => {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    if (type == "display") {
-      return (
-        (minutes < 10 ? "0" + minutes : minutes) +
-        ":" +
-        (seconds < 10 ? "0" + seconds : seconds)
-      );
-    } else {
-      return minutes;
-    }
-  };
-
-  const changeTime = (amount, type) => {
-    if (timerOn) return;
-    if (type == "break") {
-      if (
-        (breakTime <= 60 && amount < 0) ||
-        (breakTime >= 60 * 60 && amount > 0)
-      ) {
-        return;
-      }
-      setBreakTime((prev) => prev + amount);
-    } else {
-      if (
-        (sessionTime <= 60 && amount < 0) ||
-        (sessionTime >= 60 * 60 && amount > 0)
-      ) {
-        return;
-      }
-      setSessionTime((prev) => prev + amount);
     }
   };
 
   const resetTime = () => {
-    audioElement.current.load();
+    audioElement.current.pause();
+    audioElement.current.currentTime = 0;
     clearInterval(intervalId);
     setIntervalId(null);
     setDisplayTime(25 * 60);
     setBreakTime(5 * 60);
     setSessionTime(25 * 60);
-    setTimerOn(false);
     setOnBreak(false);
   };
 
+  const incSessionTime = () => {
+    if (intervalId !== null) return;
+    setSessionTime(sessionTime >= 60 * 60 ? sessionTime : sessionTime + 60);
+  };
+
+  const decSessionTime = () => {
+    if (intervalId !== null) return;
+    setSessionTime(sessionTime <= 60 ? sessionTime : sessionTime - 60);
+  };
+
+  const convertToMinutesDisplay = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
+
+  const convertToMinutesLength = (time) => {
+    return time / 60;
+  };
+
+  const incBreakTime = () => {
+    if (intervalId !== null) return;
+    setBreakTime(breakTime >= 60 * 60 ? breakTime : breakTime + 60);
+  };
+
+  const decBreakTime = () => {
+    if (intervalId !== null) return;
+    setBreakTime(breakTime <= 60 ? breakTime : breakTime - 60);
+  };
+
   return (
-    <div className="App">
-      <p id="title">Pomodoro Clock</p>
-      <div className={`timer-wrapper ${timerOn ? "fade" : ""}`}>
-        <p id="timer-label">{onBreak ? "Break" : "Session"}</p>
-        <p id="time-left">{formatTime(displayTime, "display")}</p>
+    <div id="App">
+      <h1 id="title">Pomodoro Clock</h1>
+      <div className={`timer-wrapper ${intervalId !== null ? "fade" : ""}`}>
+        <h1 id="timer-label">{onBreak ? "Break" : "Session"}</h1>
+        <h2 id="time-left">{convertToMinutesDisplay(displayTime)}</h2>
         <p>
           {onBreak ? (
             <FontAwesomeIcon id="icon-timer" icon="coffee" />
@@ -127,50 +115,46 @@ function App() {
       </div>
 
       <div className="controls-wrapper">
-        <button id="start_stop" onClick={handleStartStopClick}>
-          {timerOn ? (
+        <button id="start_stop" onClick={timerControl}>
+          {intervalId !== null ? (
             <FontAwesomeIcon icon="pause" />
           ) : (
             <FontAwesomeIcon icon="play" />
           )}
         </button>
-        <FontAwesomeIcon id="reset" icon="sync-alt" onClick={resetTime} />
+        <button id="reset" onClick={resetTime}>
+          {<FontAwesomeIcon id="reset" icon="sync-alt" />}
+        </button>
       </div>
 
       <div className="length-wrapper">
-        <LengthComponent
-          type={"session"}
-          time={sessionTime}
-          formatTime={formatTime}
-          changeTime={changeTime}
-        />
-        <LengthComponent
-          type={"break"}
-          time={breakTime}
-          formatTime={formatTime}
-          changeTime={changeTime}
-        />
+        <div className="session-wrapper">
+          <h2 id="session-label">Session Length</h2>
+          <button id="session-decrement" onClick={decSessionTime}>
+            -
+          </button>
+          <p id="session-length">{convertToMinutesLength(sessionTime)}</p>
+          <button id="session-increment" onClick={incSessionTime}>
+            +
+          </button>
+        </div>
+
+        <div className="break-wrapper">
+          <h2 id="break-label">Break Length</h2>
+          <button id="break-decrement" onClick={decBreakTime}>
+            -
+          </button>
+          <p id="break-length">{convertToMinutesLength(breakTime)}</p>
+          <button id="break-increment" onClick={incBreakTime}>
+            +
+          </button>
+        </div>
       </div>
       <audio
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
         id="beep"
         ref={audioElement}
-        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-      />
-    </div>
-  );
-}
-
-function LengthComponent({ type, time, formatTime, changeTime }) {
-  return (
-    <div className={`${type}-wrapper`}>
-      <p id={`${type}-label`}>{type} length</p>
-      <button id={`${type}-decrement`} onClick={() => changeTime(-60, type)}>
-        -
-      </button>
-      <p id={`${type}-length`}>{formatTime(time)}</p>
-      <button id={`${type}-increment`} onClick={() => changeTime(+60, type)}>
-        +
-      </button>
+      ></audio>
     </div>
   );
 }
